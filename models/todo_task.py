7bec23b53a5c418d0da0605f5a50dd24b19535d2
omarrabdoo00@gmail.com <omarrabdoo00@gmail.com>
2025-11-19 15:52:10 +0200

    Finish the relational field with res_partner model

--- models/todo_task.py
+++ models/todo_task.py
@@ -1,5 +1,3 @@
-from email.policy import default
-
 from odoo import models, fields, api
 
 
@@ -8,12 +6,13 @@ class TodoTask(models.Model):
     _description = 'Task'
 
     # Fields:
-    task_name = fields.Char(size=20,required=1)
-    task_description = fields.Text(required=1)
+    task_name = fields.Char(size=20, required=True)
+    task_description = fields.Text(required=True)
     due_date = fields.Date(required=True)
     state = fields.Selection([
-        ('new','New'),
-        ('in progress','In Progress'),
-        ('completed','Completed'),
-    ],default='new')
+        ('new', 'New'),
+        ('in progress', 'In Progress'),
+        ('completed', 'Completed'),
+    ], default='new')
 
+    assign_to = fields.Many2one('res.partner', string="Assign To")
